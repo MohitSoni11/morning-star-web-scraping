d046f0960f815072ea8ade5d27146d326e899ac3
Mohit Soni <mohitksoni1104@outlook.com>
2023-12-24 16:53:51 -0800

    Retrieving fundamental ticker info

--- app.py
+++ app.py
@@ -15,6 +15,62 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
 
 ######################
+## Helper Functions ##
+######################
+
+def login_morningstar():
+  # Starting web-scraper and making browser headless
+  options = Options()
+  #options.add_argument('--headless')
+  
+  browser = webdriver.Chrome(options=options)
+  browser.get('https://evgcatalog.kcls.org/eg/opac/ezproxy/login?qurl=https://ar.morningstar.com%2fmirc%2fhome.aspx')
+  browser.maximize_window()
+  
+  barcode_input = browser.find_element(By.ID, 'barcode-input')
+  password_input = browser.find_element(By.ID, 'password-input')
+  barcode_input.send_keys('9340044928')
+  password_input.send_keys('0035')
+  password_input.submit()
+  
+  # Waiting for at most 30 seconds to allow morningstar to load
+  browser.implicitly_wait(30)
+  return browser
+
+def get_ticker_info(browser, ticker):
+  data = []
+  
+  # Searching for ticker
+  search_bar = browser.find_element(By.XPATH, "//input[@class='mdc-search-field__input mds-search-field__input__rxp']")
+  search_bar.send_keys(ticker)
+  search_bar.submit()
+  
+  # Go to ticker page
+  ticker_button = browser.find_element(By.XPATH, "//a[@class='mdc-security-module__name mdc-link__rxp mdc-link--no-underline__rxp']")
+  ticker_button.click()
+  
+  # Getting information under Morningstar Fundamentals (except the Sustainability section)
+  data += get_fundamental_info(browser)
+    
+  return data
+
+def get_fundamental_info(browser):
+  data = []
+  fundamental_section = browser.find_element(By.ID, 'sal-components-eqsv-overview')
+  
+  # Looping through first three lists in fundamental_section and getting all data
+  for i in range(3):
+    lists = fundamental_section.find_elements(By.CSS_SELECTOR, 'ul')[i]
+    
+    for element in lists.find_elements(By.CSS_SELECTOR, 'p.sal-dp-value'):
+      data.append(element.text)
+      
+    for element in lists.find_elements(By.CSS_SELECTOR, 'span.sal-dp-value'):
+      data.append(element.text)
+  
+  return data
+
+######################
 ## Initializing App ##
 ######################
 
@@ -25,6 +81,7 @@ app = Flask(__name__)
 ######################
 
 tickers = []
+browser = login_morningstar()
 
 #############
 ## Routing ##
@@ -37,35 +94,4 @@ def home():
 @app.route('/add-ticker', methods=['POST'])
 def addTicker():
   tickers.append(request.form['ticker'])
-  get_info(request.form['ticker'])
-  return redirect('/')
-
-def get_info(ticker):
-  # Starting web-scraper and making browser headless
-  options = Options()
-  options.add_argument('--headless')
-  
-  browser = webdriver.Edge(options=options)
-  browser.get('https://evgcatalog.kcls.org/eg/opac/ezproxy/login?qurl=https://ar.morningstar.com%2fmirc%2fhome.aspx')
-  browser.maximize_window()
-  
-  # Login to morningstar
-  barcode_input = browser.find_element(By.ID, 'barcode-input')
-  password_input = browser.find_element(By.ID, 'password-input')
-  barcode_input.send_keys('9340044928')
-  password_input.send_keys('0035')
-  password_input.submit()
-  
-  # Waiting for at most 30 seconds to allow morningstar to load
-  browser.implicitly_wait(30)
-  
-  # Go to ticker page
-  search_bar = browser.find_element(By.XPATH, "//input[@class='mdc-search-field__input mds-search-field__input__rxp']")
-  search_bar.send_keys(ticker)
-  search_bar.submit()
-  
-  ticker_button = browser.find_element(By.XPATH, "//a[@class='mdc-security-module__name mdc-link__rxp mdc-link--no-underline__rxp']")
-  ticker_button.click()
-  
-  for element in browser.find_elements(By.XPATH, "//p[@class='sal-dp-value']"):
-    print(element.text + ",")
+  return redirect('/', data=get_ticker_info(browser, request.form['ticker']))
